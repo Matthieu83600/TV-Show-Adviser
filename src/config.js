5be464f830fdd9d4dac5a855b3f892b82a2d1e72
Matthieu Bonjour <matthieu83600-pro@outlook.fr>
2024-09-20 21:41:52 +0200

    fix: api key

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const BASE_URL = "https://api.themoviedb.org/3/";
-const API_KEY_PARAM = "?api_key=3bea5da01b9928b42a5526056c6c3005";
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY_PARAM = `?api_key=${API_KEY}`;
 const BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/original/";
 const SMALL_IMG_COVER_BASE_URL = "https://image.tmdb.org/t/p/w300/";
 
